fb79d1a83bffb2ebe1f80c63f70bf158d25b08db
filypsdias <lipao.dias@hotmail.com>
2018-06-19 19:16:34 -0300

    Improving the test file

--- diskspace/test.py
+++ diskspace/test.py
@@ -1,5 +1,35 @@
+from diskspace import bytes_to_readable, subprocess_check_output, 
+from diskspace import print_tree, calculate_percentage, du_command, percentage_args
 import unittest
+import mock
+import io
+import sys
+import os
+import subprocess
+import StringIO
 
-from diskspace import diskspace
 
-class test(unittest.TestCase):
+
+class DiskspaceTest(unittest.TestCase):
+
+    def setUp(self):
+        self.file_tree = {'/home/teste': {'print_size': '2.00Kb',
+                                          'children': [], 'size': 4}}
+        self.file_tree_node = {'print_size': '2.00Kb',
+                               'children': [], 'size': 4}
+        self.path = '/home/teste'
+        self.largest_size = 6
+        self.total_size = 4
+
+    def bytes_to_readable_test(self):
+        blocks = 224
+        result = "112.00Kb"
+        self.assertEqual(bytes_to_readable(blocks), result)
+
+    def subprocess_check_output_test(self):
+        command = 'du'
+        du_result = subprocess.check_output(command)
+        result = subprocess_check_output(command)
+        self.assertEqual(du_result, result)
+
+  
